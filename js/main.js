ec79341f297e5ef0b0e521a63db7955d684395c5
ESychev <375298357706@ya.ru>
2022-05-21 16:53:22 +0300

    add block design and video

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
+$(function() {
+    $('.design-slider').slick({
+        dots: false,
+        slidesToShow: 4,
+        variablewidth: true,
+        prevArrow: `<img class="arrow arrow-left" src="./images/Arrow-left.svg" alt="">`,
+        nextArrow: '<img class="arrow arrow-right" src="./images/Arrow-right.svg" alt="">',
 
+    });
 });
